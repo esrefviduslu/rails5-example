124bd95fc4356f5629181b72944c6b708111ab22
Esref Viduslu <esref.viduslu@lab2023.com>
2017-06-06 19:39:27 +0000

    Fix broken show article spec

--- spec/features/show_article_spec.rb
+++ spec/features/show_article_spec.rb
@@ -3,7 +3,9 @@ require 'rails_helper'
 RSpec.feature "Showing an article" do
   
   before do
-    @article = Article.create(title: "The first article", body: "Lorem ipsum dolor sit amet, consectetur.")
+    @john = User.create(email: "john@example.com", password: "12341234")
+    login_as(@john)
+    @article = Article.create(title: "The first article", body: "Lorem ipsum dolor sit amet, consectetur.", user: @john)
   end
   
   scenario "A user shows an article" do
